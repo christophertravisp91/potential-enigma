ea7980d71a53f102bf8302f09694c80c94be431d
TomFoolery <christophertravisp91@gmail.com>
2022-01-08 15:48:00 -0500

    added functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./develop/utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?'
+        name: 'Title',
+        message: 'What is the title of your project?'
     },
     {
         type: 'input',
@@ -37,10 +38,26 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+        return console.log(err);
+        }
+    
+        console.log("Congratulations, you have created a README file!")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {   
+        fs.writeFileSync("ReadMe.md", inquirerResponse, data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,42 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  # Table of Content
+-[description](#description)
+-[installation](#installation)
+-[usage](#usage)
+-[licenses](#licenses)
+-[contribution](#contribution)
+-[test](#test)
+-[username](#username)
+-[profile](#profile)
 
+${response.username}
+##username:
+
+    ${response.description}
+##description:
+
+    ${response.installation}
+##installation:
+
+    ${response.usage}
+##usage:
+
+    ${response.licenses}
+##licenses:
+
+    ${response.contribution}
+##contribution:
+
+    ${response.test}
+##test:
+
+    ${response.email}
+##email:
+
+    ${response.profile}
+##profile:
 `;
 }
 
